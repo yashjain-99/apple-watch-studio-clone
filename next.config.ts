62c7437d58b5adf55a4246607d664ceef1071ebd
Yash Jain <91678870+yashjain-99@users.noreply.github.com>
2024-12-15 11:54:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,13 @@ eslint: {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
