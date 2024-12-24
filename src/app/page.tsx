775b2ada41f7932fcce25aee4219757f369dd9de
yashjain-99 <yashj133.yj@gmail.com>
2024-12-24 15:24:57 +0530

    Fix build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,13 @@
 "use client";
 
-import { useEffect, useState, useCallback } from "react";
-import Header from "@/components/header";
-import Intro from "@/components/intro";
-import Info from "@/components/info";
-import Footer from "@/components/footer";
-import CollectionModal from "@/components/collection-modal";
-import {
-  setBand,
-  setCase,
-  setCollection,
-  setSize,
-  useStateContext,
-} from "@/context/StateProvider";
-import { useSearchParams } from "next/navigation";
-import { getData } from "@/utils";
-import { Band, Case, Collection, Size } from "../..";
+import { Suspense } from "react";
+
+import Main from "@/components/main";
 
 export default function Home() {
-  const [isIntroPage, setIsIntroPage] = useState<boolean>(true);
-  const [isBannerImgLoaded, setIsBannerImgLoaded] = useState<boolean>(false);
-  const { dispatch } = useStateContext();
-  const searchParams = useSearchParams();
-
-  const updateStateFromParams = useCallback(() => {
-    const { collection, size, caseType, band } = Object.fromEntries(
-      searchParams.entries()
-    );
-
-    if (collection || size || caseType || band) {
-      setIsIntroPage(false);
-      setIsBannerImgLoaded(true);
-    }
-
-    const data = getData(
-      collection as Collection,
-      size as Size,
-      caseType as Case,
-      band as Band
-    );
-
-    if (data) {
-      setCollection(dispatch, data.COLLECTION);
-      setSize(dispatch, data.SIZE);
-      setCase(dispatch, data.CASE);
-      setBand(dispatch, data.BAND);
-    }
-  }, [searchParams, dispatch]);
-
-  useEffect(() => {
-    updateStateFromParams();
-  }, [searchParams, updateStateFromParams]);
-
   return (
-    <main
-      className="relative h-screen min-h-9 min-w-[400px] overflow-x-hidden overflow-y-scroll"
-      id="main"
-    >
-      <Header isBannerImgLoaded={isBannerImgLoaded} isIntroPage={isIntroPage} />
-      {isIntroPage ? (
-        <Intro setIsIntroPage={setIsIntroPage} />
-      ) : (
-        <>
-          <Info
-            isBannerImgLoaded={isBannerImgLoaded}
-            setIsBannerImgLoaded={setIsBannerImgLoaded}
-          />
-          {isBannerImgLoaded && <Footer />}
-        </>
-      )}
-      <CollectionModal />
-    </main>
+    <Suspense fallback={<div>Loading ...</div>}>
+      <Main />
+    </Suspense>
   );
 }
---
+++ src/components/main.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useEffect, useState, useCallback } from "react";
+import Header from "@/components/header";
+import Intro from "@/components/intro";
+import Info from "@/components/info";
+import Footer from "@/components/footer";
+import CollectionModal from "@/components/collection-modal";
+import {
+  setBand,
+  setCase,
+  setCollection,
+  setSize,
+  useStateContext,
+} from "@/context/StateProvider";
+import { useSearchParams } from "next/navigation";
+import { getData } from "@/utils";
+import { Band, Case, Collection, Size } from "../..";
+
+const Main = () => {
+  const [isIntroPage, setIsIntroPage] = useState<boolean>(true);
+  const [isBannerImgLoaded, setIsBannerImgLoaded] = useState<boolean>(false);
+  const { dispatch } = useStateContext();
+  const searchParams = useSearchParams();
+
+  const updateStateFromParams = useCallback(() => {
+    const { collection, size, caseType, band } = Object.fromEntries(
+      searchParams.entries()
+    );
+
+    if (collection || size || caseType || band) {
+      setIsIntroPage(false);
+      setIsBannerImgLoaded(true);
+    }
+
+    const data = getData(
+      collection as Collection,
+      size as Size,
+      caseType as Case,
+      band as Band
+    );
+
+    if (data) {
+      setCollection(dispatch, data.COLLECTION);
+      setSize(dispatch, data.SIZE);
+      setCase(dispatch, data.CASE);
+      setBand(dispatch, data.BAND);
+    }
+  }, [searchParams, dispatch]);
+
+  useEffect(() => {
+    updateStateFromParams();
+  }, [searchParams, updateStateFromParams]);
+  return (
+    <main
+      className="relative h-screen min-h-9 min-w-[400px] overflow-x-hidden overflow-y-scroll"
+      id="main"
+    >
+      <Header isBannerImgLoaded={isBannerImgLoaded} isIntroPage={isIntroPage} />
+      {isIntroPage ? (
+        <Intro setIsIntroPage={setIsIntroPage} />
+      ) : (
+        <>
+          <Info
+            isBannerImgLoaded={isBannerImgLoaded}
+            setIsBannerImgLoaded={setIsBannerImgLoaded}
+          />
+          {isBannerImgLoaded && <Footer />}
+        </>
+      )}
+      <CollectionModal />
+    </main>
+  );
+};
+
+export default Main;
